1f94d842e176fdeb88f3592fc0b97977d627fdbb
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-05-22 09:53:24 -0300

    feat: create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './global.css'
+import styles from './App.module.css';
+
 import {Header} from "./components/Header.jsx";
+import {Sidebar} from "./components/Sidebar.jsx";
 
 export function App() {
   return (
     <>
         <Header />
+        <div className={styles.wrapper}>
+            <Sidebar />
+            <main>
+                <h1>Main Content</h1>
+            </main>
+        </div>
     </>
   )
 }
