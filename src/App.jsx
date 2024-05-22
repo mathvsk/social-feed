fabc317d1a1638ef58cbeb9dde1201aa37d51662
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-05-22 19:31:41 -0300

    refactor: add dynamic post simulation.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,39 @@ import {Header} from "./components/Header.jsx";
 import {Sidebar} from "./components/Sidebar.jsx";
 import {Post} from "./components/Post.jsx";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      name: 'Julio Alcantara',
+      avatarUrl: 'https://i.pravatar.cc/150?img=11',
+      role: 'Java Senior Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Pessoal, alguém sabe se a Rocketseat está contratando?' },
+      { type: 'paragraph', content: 'Estou procurando uma oportunidade de emprego como desenvolvedor Java Senior.' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: 'hashTag', content: ['#rocketseat', '#job', '#java'] }
+    ],
+    publishedAt: new Date('2024-05-20 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      name: 'Gabriel Lisboa',
+      avatarUrl: 'https://i.pravatar.cc/150?img=9',
+      role: 'ReactJS Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera, beleza?' },
+      { type: 'paragraph', content: 'Estou fazendo o bootcamp da Rocketseat e está sendo muito massa! Alguém mais aí fazendo, comenta na publicação para trocarmos uma ideia.' },
+      { type: 'link', content: 'example.design/doctorcare' },
+      { type: 'hashTag', content: ['#rocketseat', '#nlw', '#nextlevelweek', "#ReactJS"] }
+    ],
+    publishedAt: new Date('2024-05-21 20:00:00'),
+  }
+]
+
 export function App() {
   return (
     <>
@@ -12,7 +45,15 @@ export function App() {
         <div className={styles.wrapper}>
             <Sidebar />
             <main>
-                <Post />
+              {posts.map(post => {
+                return (
+                  <Post
+                    author={post.author}
+                    content={post.content}
+                    publishedAt={post.publishedAt}
+                  />
+                )
+              })}
             </main>
         </div>
     </>
